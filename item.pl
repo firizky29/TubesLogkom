05ba617a280afaf495b1f2cbec65e99bb49e09f0
Gleen <rahmatrafid22@gmail.com>
2021-11-22 15:02:24 +0700

    Create item.pl

---
+++ item.pl
@@ -0,0 +1,38 @@
+itemType(fish).
+itemType(gardening).
+itemType(equipment).
+itemType(seed).
+	
+inventory(arwana, fish, 0).
+inventory(gurame, fish, 0).
+inventory(lele, fish, 0).
+inventory(mujair, fish, 0).
+inventory(nila, fish, 0).
+	
+price(arwana, 100).
+price(gurame, 6).
+price(lele, 3).
+price(mujair, 4).
+price(nila, 5).
+	
+inventory(wortel, gardening, 0).
+inventory(lobak, gardening, 0).
+inventory(kentang, gardening, 0).
+inventory(bawang, gardening, 0).
+inventory(tomat, gardening, 0).
+
+price(wortel, 10).
+price(lobak, 5).
+price(kentang, 7).
+price(bawang, 15).
+price(tomat, 8).
+	
+inventory(fishing_rod_1,equipment,0).
+inventory(fishing_rod_2,equipment,0).
+inventory(fishing_rod_3,equipment,0).
+inventory(hoe_1,equipment,0).
+inventory(hoe_2,equipment,0).
+inventory(hoe_3,equipment,0).
+inventory(watering_1,equipment,0).
+inventory(watering_2,equipment,0).
+inventory(watering_3,equipment,0).

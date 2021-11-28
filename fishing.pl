1e44eedb06c9f8f65de087e1925de988d977b452
firizky29 <13520095@std.stei.itb.ac.id>
2021-11-28 19:33:01 +0700

    fishing mechanism

--- fishing.pl
+++ fishing.pl
@@ -1,46 +1,52 @@
 isAroundWater:-
-    tile(XP, YP, player),
+    playerLoc(XP, YP),
     X1 is XP-1,
-    X2 is XP+1,
-    Y1 is YP-1, 
-    Y2 is YP+1,
-    (tile(X1, YP, water); (tile(X2, YP, water)); (tile(XP, Y1, water)); tile(XP, Y2, water)), !.
+    tile(X1, YP, water), 
+    !.
 
-getFish(FishFished):-
-    random(0,120,R),
-    R =< 20,
-    FishFished is empty, !.
-getFish(FishFished):-
-    random(0,120,R),
+isAroundWater:-
+    playerLoc(XP, YP),
+    X2 is XP+1,
+    tile(X2, YP, water),
+    !.
+
+isAroundWater:-
+    playerLoc(XP, YP),
+    Y1 is YP-1, 
+    tile(XP, Y1, water),
+    !.
+
+isAroundWater:-
+    playerLoc(XP, YP),
+    Y2 is YP+1,
+    tile(XP, Y2, water), 
+    !.
+
+getFish(R, empty):-
+    R =< 20, !.
+getFish(R, arwana):-
     R > 20,
-    R =< 30,
-    FishFished is arwana, !.
-getFish(FishFished):-
-    random(0,120,R),
+    R =< 30, !.
+getFish(R, gurame):-
     R > 30,
-    R =< 45,
-    FishFished is gurame, !.
-getFish(FishFished):-
-    random(0,120,R),
+    R =< 45, !.
+getFish(R, lele):-
     R > 45,
-    R =< 75,
-    FishFished is lele, !.
-getFish(FishFished):-
-    random(0,120,R),
+    R =< 75, !.
+getFish(R, nila):-
     R > 75,
-    R =< 100,
-    FishFished is nila, !.
-getFish(FishFished):-
-    random(0,120,R),
-    R > 100,
-    FishFished is mujair, !.
+    R =< 100, !.
+getFish(R, mujair):-
+    R > 100, !.
 
 fish :-
     isAroundWater,
-    getFish(FishFished),
-    retract(inventory(FishFished, fish, Previous)),
-    New is Previous + 1,
-    asserta(inventory(FishFished, fish, New)),
+    random(0,120,R),
+    getFish(R, FishFished),
+    % write(FishFished),
+    % retract(inventory(FishFished, fish, Previous)),
+    % New is Previous + 1,
+    % asserta(inventory(FishFished, fish, New)),
     write('You got a '),
     write(FishFished),
     write('!'),nl,!.

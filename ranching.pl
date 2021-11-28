7714a041ef8aca43113972b747e17c30720aa3a6
averrous-s <16520480@std.stei.itb.ac.id>
2021-11-28 19:40:40 +0700

    Resolve bugs on compile time because of ranching

--- ranching.pl
+++ ranching.pl
@@ -4,7 +4,6 @@
 
 % TODO: Integrasi daynya
 
-day(0)
 ranch_animal(ayam).
 ranch_animal(sapi).
 ranch_animal(domba).
@@ -16,6 +15,7 @@ animal_count(domba,0).
 last_ranch_visit(0).
 
 ranch :-
+    playerLoc(X,Y),
     tile(X,Y,ranch),
     write('Welcome to the ranch! You have:'),nl,
     forall(ranch_animal(Animal),
@@ -23,7 +23,7 @@ ranch :-
                animal_count(Animal, Count),write(Count),write(Animal),nl
            )
           ),
-    write('What do you want to do?'),nl.
+    write('What do you want to do?'),nl,!.
 % TODO: change egg production logic to depend on chickens's age
 eggProduction(M) :-
     day(D),
@@ -31,7 +31,7 @@ eggProduction(M) :-
     Diff is D - LastVisit,
     animal_count(ayam,AyamCount),
     random(0,0.8,P),
-    M is AyamCount * Diff * round(2 * P).
+    M is AyamCount * Diff * round(2 * P), !.
 
 % TODO: change wool production logic to depend on sheep's age
 woolProduction(M) :-
@@ -40,7 +40,7 @@ woolProduction(M) :-
     Diff is D - LastVisit,
     animal_count(domba,DombaCount),
     random(0,0.4,P),
-    M is DombaCount * Diff * round(2 * P).
+    M is DombaCount * Diff * round(2 * P), !.
 
 % TODO: Change meat production logic to counts of the cow and its ages
 meatProduction(M) :-
@@ -49,9 +49,10 @@ meatProduction(M) :-
     Diff is D - LastVisit,
     animal_count(sapi,SapiCount),
     random(0,0.2,P),
-    M is SapiCount * Diff * round(2 * P).
+    M is SapiCount * Diff * round(2 * P), !.
 
 ayam :-
+    playerLoc(X,Y),
     tile(X,Y,ranch),
     write('Your chicken lays '),
     eggProduction(M),
@@ -66,20 +67,41 @@ ayam :-
     write(' new eggs.'),nl,
     write('Now, you have '),
     write(NewEggs),
-    write(' eggs.'),nl.
+    write(' eggs.'),nl,
     ranchingExp(PreviousExp),
     NewExp is PreviousExp + 6,
     retract(ranchingExp(_)),
     asserta(ranchingExp(NewExp)),
-    write('You gain 6 ranching exp').
+    write('You gain 6 ranching exp'), !.
 
 
 domba :-
+    playerLoc(X,Y),
     tile(X,Y,ranch),
+    woolProduction(M),
+    M > 0,
+    write('Your sheep produces '),
+    write(M),
+    write(' kg of wool.'),nl,
+    inventory(sutra, produce, ExistingWool),
+    NewWool is ExistingWool + M,
+    retract(inventory(sutra, produce, _)),
+    asserta(inventory(sutra, produce, NewWool)),
+    write('You got '),
+    write(M),
+    write(' new kg of wool.'),nl,
+    write('Now, you have '),
+    write(NewWool),
+    write(' kg of wool.'),nl,
+    ranchingExp(PreviousExp),
+    NewExp is PreviousExp + 6,
+    retract(ranchingExp(_)),
+    asserta(ranchingExp(NewExp)),
+    write('You gain 6 ranching exp'), !.
 
     
 
 sapi :-
-    tile(X,Y,ranch).
-    % gotta research how sapis reproduce
-    
+    playerLoc(X,Y),
+    tile(X,Y,ranch), !.
+
